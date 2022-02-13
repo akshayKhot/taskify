78140180c2120e43b90495176ae89340c642b373
Akshay Khot <akshay.khot@hey.com>
2022-02-12 18:00:01 -0800

    implement task manager

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,5 +2,31 @@ class TasksController < ApplicationController
 
   def index
     @tasks = Task.all
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+
+    respond_to do |format|
+      if @task.save
+        format.html { redirect_to tasks_url, notice: "Task was successfully created" }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def toggle
+    @task = Task.find(params[:id])
+    @task.update(completed: params[:completed])
+
+    render json: { message: "Success" }
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:description)
   end
 end
