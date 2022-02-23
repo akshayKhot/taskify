78cc4dcfb80ae52d9e6b89d566cd9469bc5efb74
Akshay Khot <akshay.khot@hey.com>
2022-02-22 16:01:28 -0800

    edit and delete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -17,6 +17,29 @@ class TasksController < ApplicationController
     end
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+
+    respond_to do |format|
+      if @task.update(task_params)
+        format.html { redirect_to tasks_url, notice: "Task was successfully updated" }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    redirect_to tasks_url, notice: "Task was successfully deleted."
+  end
+
   def toggle
     @task = Task.find(params[:id])
     @task.update(completed: params[:completed])
